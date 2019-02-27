51f3fd373a214fc1e16f285a844130c34dbdfd29
sophieyu <yusufen10@gmail.com>
2019-02-27 16:01:05 +0000

    Made corrections

---
+++ README.md
@@ -0,0 +1,30 @@
+# Getting started with Git
+
+1. Initialize the folder
+
+```
+git init
+```
+2. Add files to version control
+
+```
+git add .
+```
+
+3. Make a commit
+
+```
+git commit -am "Initial commit"
+```
+4. Remove unwanted files from version control
+
+```
+git rm test Makefile *.o *.pro.user
+```
+
+Then commit
+
+```
+git commit -am "removed unwanted files"
+```
+5. The .gitignore file
--- Vec3Tests.cpp
+++ Vec3Tests.cpp
@@ -4,7 +4,39 @@
 using namespace ::testing;
 // for more details on gtest see here https://github.com/google/googletest/blob/master/googletest/docs/primer.md
 // my lecture https://nccastaff.bournemouth.ac.uk/jmacey/PP/slides/testing/slides.html#/
-TEST(Vec3, fail)
+TEST(Vec3, testDefaultConstructor)
 {
-    ASSERT_EQ(0,1);
+    Vec3 a;
+    ASSERT_FLOAT_EQ(a.m_x,0.0f);
+    ASSERT_FLOAT_EQ(a.m_y,0.0f);
+    ASSERT_FLOAT_EQ(a.m_z,0.0f);
+
+}
+
+TEST(Vec3,userDefinedConstructor)
+{
+    Vec3 a(1.0f,2.0f,3.0f);
+    ASSERT_FLOAT_EQ(a.m_x,1.0f);
+    ASSERT_FLOAT_EQ(a.m_y,2.0f);
+    ASSERT_FLOAT_EQ(a.m_z,3.0f);
+
+
+}
+
+void Vec3::set(float _x,float _y, float _z)
+{
+    m_x = _x;
+    m_y = _y;
+    m_z = _z;
+}
+
+TEST(Vec3,set)
+{
+    Vec3 a;
+    a.set(1.0f,2.0f,3.0f);
+    ASSERT_FLOAT_EQ(a.m_x,1.0f);
+    ASSERT_FLOAT_EQ(a.m_y,2.0f);
+    ASSERT_FLOAT_EQ(a.m_z,3.0f);
+
+
 }
